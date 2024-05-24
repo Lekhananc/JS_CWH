eb71341caac9774c6f2cccbb1bd728b27918b06c
Lekhananc <86131162+Lekhananc@users.noreply.github.com>
2024-05-24 09:32:00 +0000

    Strings

---
+++ 13_Strings.js
@@ -0,0 +1,22 @@
+let name = 'lekhana'
+console.log(name.length);
+console.log(name[0]);
+console.log(name[3]);
+console.log(name[6]);
+let friend = "sharada"
+console.log(friend);
+
+console.log();
+
+let boy1 = "pramod"
+let boy2 = "harry"
+
+console.log(`${boy2} is a friend of ${boy1}`);
+
+console.log();
+// Escape sequence characters
+
+let name1 = 'lekhana\'s'
+console.log(name1);
+
+console.log('Hello\nGood\nMorning');
---
+++ 14_StringMethods.js
@@ -0,0 +1,11 @@
+let name1 = 'lekhana\'s';
+console.log(name1.length);
+
+let name = 'lekhana';
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.indexOf('k'));
+console.log(name.replace('lek',('sss')));
+
+let lastName = ' chidanand';
+console.log(name.concat(lastName));
